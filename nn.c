3c85223db0219d9e948b96c2c3d69ac61976039f
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-02-06 16:20:20 +0530

    Create nn.c

---
+++ nn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x;
+printf("enter the chatecter");
+scanf("%d",&x);
+if(x%4==0)
+{
+printf("leap year");
+}
+else
+{
+printf("not leap year");
+}
+return 0;
+}
