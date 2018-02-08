ba8e9fbc6fa75f38635c3eca2b462d089676e6cb
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-02-08 16:05:28 +0530

    Create 8ni2.c

---
+++ 8ni2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+int j,sum=0,c;
+printf(" Enter the values");
+scanf("%d",&c);
+for(j=1;j<=c;j++)
+{
+sum+=a;
+printf("%d",j);
+}
+	return 0;
+}
