f8bbdd49edc6292b23720e698bfd66887edef7be
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-02-08 16:23:25 +0530

    Create 8ni5.c

---
+++ 8ni5.c
@@ -0,0 +1,21 @@
+include<stdio.h>
+int main()
+{
+int num,a,b,c,p,q;
+printf("enter the number less than or equal to 1000 :");
+scanf("%d",&num);
+a=num/100;
+b=num%100;
+c=b/10;
+d=num%10;
+p=q*100+c*10+a;
+if(num==e)
+{
+printf("palindrome");
+}
+else
+{
+printf("not a palindrome");
+}
+return 0;
+}
