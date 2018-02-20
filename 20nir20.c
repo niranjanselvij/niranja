22c43bc9d98ba7cae1e7807e04d10eb91837d0da
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-02-20 16:06:53 +0530

    Create 20nir20.c

---
+++ 20nir20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n, j;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+
+    for(j=1; j<=10; ++j)
+    {
+       printf("\n%d",n*j);
+    }
+}
