c8bbafb085b9ef19423beb667f270bdb75e4a48e
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-02-20 16:01:29 +0530

    Create 20nir19.c

---
+++ 20nir19.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,j;
+printf("enter the integer number");
+scanf("%d",&n);
+if(n%2=0)
+printf("enter the factorial negative number");
+for(j=1;j<1;j++)
+{
+factorial*=1;
+}
+else
+printf("enter the not factorial number");
+}
+
