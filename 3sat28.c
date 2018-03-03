f286f8ed7c8c689cbab70c526e23846592d03cc4
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-03-03 14:56:18 +0530

    Create 3sat28.c

---
+++ 3sat28.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b[10],j,n;
+printf("enter the limimt");
+scanf("%d",&n);
+printf("enter the element");
+for(j=0;j<n;j++)
+{
+scanf("%d",&b[i]);
+}
+printf("the elements with index");
+for(j=0;j<n;j++)
+{
+printf("%d\t%d\n",j,b[i]);
+}
+}
