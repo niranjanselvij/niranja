515152f67c846d0421f6786c229297ec474c6dbb
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-03-15 16:35:06 +0530

    Create thur44.c

---
+++ thur44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if((a>1)&&(a<10))
+{
+printf("%d",a);
+}
+else
+{
+    printf("not in range");
+}
+}
