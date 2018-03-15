151869b16bb0fd58c212ab03e8667a66e2e7ea20
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-03-15 16:01:28 +0530

    Create thru41.c

---
+++ thru41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[5];
+  int a,j;
+printf("enter a string");
+scanf("%s",&str);
+printf("enter a limit");
+scanf("%d",&a);
+for(j=0;j<=a;j++)
+{
+printf("%s",str);
+}
+}
