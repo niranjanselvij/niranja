d671406834cabea34ffdbda345554ae629f99d6c
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-03-16 14:56:59 +0530

    Create fri50.c

---
+++ fri50.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int e,f,b=0;
+scanf("%d",&e);
+for(f=1;f<e;f++)
+{
+if(pow(2,f)==e)
+{
+c=1;
+}
+}
+if(b==1)
+printf("\n YES");
+else
+printf("\n NO");
+return 0;
+}
